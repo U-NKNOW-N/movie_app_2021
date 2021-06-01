2b5b14908f8758ccd29f231340922de56b712779
U-NKNOW-N <m1t0yr@gmail.com>
2021-06-01 18:16:18 +0900

    Movie Detail Update

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Detail.css";
 
 class Detail extends React.Component {
   componentDidMount() {
@@ -6,13 +7,35 @@ class Detail extends React.Component {
     if (location.state === undefined) {
       history.push("/");
     }
-    console.log(location.state);
   }
   render() {
     const { location } = this.props;
     if (location.state) {
-      return <span>{location.state.title}</span>;
-    } else {
+      console.log(location.state);
+      return (
+        <div className="detail">
+          <h3 className="datail__title">{location.state.title}</h3>
+          <div className="datail__year">({location.state.year})</div>
+          <div className="datail__poster">
+          <img src={location.state.poster} alt={location.state.title} title={location.state.title} /></div>
+          <div className="datail__name">
+          Genres
+          </div>
+          <div className="datail__content">
+          {location.state.genres.map((genre, index) => (
+            <div key={index}>
+              {genre}&nbsp;
+            </div>
+          ))}
+          </div>
+          <div className="datail__name">
+          Summary
+          </div>
+          <div className="datail__content">
+          {location.state.summary}
+          </div>
+      </div>
+      )} else {
       return null;
     }
   }
